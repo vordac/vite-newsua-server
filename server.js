24798cfa31d95942810234d7506821b2fc6b1bd1
vordac <oleksandr.burgazli@gmail.com>
2024-05-24 15:10:46 +0300

    1.2

--- server.js
+++ server.js
@@ -20,6 +20,11 @@ app.use(cors());
 //   credentials: true,
 //   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 // }));
+// app.use(cors({
+//   origin: 'http://localhost:3000/',
+//   credentials: true,
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }));
 app.use(express.json());
 
 app.get('/articles-grid', gridArticlesController.getArticlesGrid);
